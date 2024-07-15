3a22affaf05842b1ce763ef84c0724bdbc316722
Paul Tapping <zombieplayground@gmail.com>
2024-07-15 23:22:13 +0100

    BUILD: models.py ready to import to postgres

--- quest_log/models.py
+++ quest_log/models.py
@@ -1,26 +1,29 @@
-from datetime import datetime
+from datetime import datetime, timezone
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, CheckConstraint
 from quest_log import db
 
 class User(db.Model):
     user_id =  db.Column(db.Integer, primary_key = True, nullable = False) 
     username =  db.Column(db.String(50), unique = True, nullable = False) 
     email =  db.Column(db.String(50), unique = True, nullable = False)
-    password_hash =  db.Column(db.String(100), nullable = False) 
+    password_hash =  db.Column(db.String(20), nullable = False) 
     location =  db.Column(db.String(50), nullable = True) 
-    created_at = db.Column(db.DateTime, default = datetime.now(timezone.utc), nullable = False)
-    avatar_url = db.Column(db.String(50), nullable = True)
+    created_at = db.Column(db.DateTime, default = lambda: datetime.now(timezone.utc), nullable = False)
+    avatar_url = db.Column(db.String(512), nullable = True)
 
 class Game(db.Model):
     game_id = db.Column(db.Integer, primary_key = True)
     game_title = db.Column(db.String(50), unique = True, nullable = False)
     game_publisher = db.Column(db.String(50), nullable = False)
     developer = db.Column(db.String(50), nullable = False) 
-    release_date = db.Column(db.DateTime,  default = datetime.now(timezone.utc), nullable = False) 
+    release_date = db.Column(db.DateTime,  default = lambda: datetime.now(timezone.utc), nullable = False) 
     genre = db.Column(db.String(50), nullable = False)
-    image_url = db.Column(db.String(50), nullable = False)
-    rating = db.Column(db.Float)
+    image_url = db.Column(db.String(512), nullable = False)
+    rating = db.Column(db.Float, nullable = True)
+    __table_args__ = (
+        CheckConstraint('rating >= 0 AND rating <= 10', name='check_rating_range'),
+    )
 
     def __repr__(self):
         return self.game_title
@@ -32,16 +35,20 @@ class Review(db.Model):
     review_content = db.Column(db.Text, nullable = False)
     hours_played = db.Column(db.Integer, nullable = False) 
     completed = db.Column(db.Boolean, nullable = True) 
-    rating = db.Column(db.Integer, nullable = False) #must be between 0 - 100 inclusive
-    review_created_at = db.Column(db.DateTime, nullable = False, default = datetime.now(timezone.utc)) 
+    rating = db.Column(db.Integer, nullable = False) #must be between 0 - 10 inclusive
+    review_created_at = db.Column(db.DateTime, nullable = False, default = lambda: datetime.now(timezone.utc)) 
     
     game = db.relationship('Game', backref=db.backref('reviews', lazy=True))
     user = db.relationship('User', backref=db.backref('reviews', lazy=True))
+    __table_args__ = (
+            CheckConstraint('rating >= 0 AND rating <= 100', name='check_rating_range'),
+            )
+
 
 class UserGame(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
     game_id = db.Column(db.Integer, db.ForeignKey('game.game_id'), primary_key=True)
-    added_at = db.Column(db.DateTime, nullable=False, default = datetime.now(timezone.utc))
+    added_at = db.Column(db.DateTime, nullable=False, default = lambda: datetime.now(timezone.utc))
      
     game = db.relationship('Game', backref=db.backref('user_games', lazy=True))
     user = db.relationship('User', backref=db.backref('user_games', lazy=True))
--- quest_log/routes.py
+++ quest_log/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from quest_log import app, db
+from quest_log.models import Game, Review, User, UserGame
 
 @app.route("/")
 def home():
