833ecb5ee93187a8bd541f9b2073f0b4b59ec2c2
Paul Tapping <zombieplayground@gmail.com>
2024-07-15 22:30:21 +0100

    BUILD: initial setup of database schema in models.py

--- quest_log/models.py
+++ quest_log/models.py
@@ -9,16 +9,17 @@ class User(db.Model):
     email =  db.Column(db.String(50), unique = True, nullable = False)
     password_hash =  db.Column(db.String(100), nullable = False) 
     location =  db.Column(db.String(50), nullable = True) 
-    created_at = db.Column(db.DateTime, nullable = False) 
+    created_at = db.Column(db.DateTime, default = datetime.now(timezone.utc), nullable = False)
+    avatar_url = db.Column(db.String(50), nullable = True)
 
 class Game(db.Model):
     game_id = db.Column(db.Integer, primary_key = True)
     game_title = db.Column(db.String(50), unique = True, nullable = False)
     game_publisher = db.Column(db.String(50), nullable = False)
     developer = db.Column(db.String(50), nullable = False) 
-    release_date = db.Column(db.DateTime, nullable = False) 
+    release_date = db.Column(db.DateTime,  default = datetime.now(timezone.utc), nullable = False) 
     genre = db.Column(db.String(50), nullable = False)
-    image_url = db.Column(db.String(512), nullable = False)
+    image_url = db.Column(db.String(50), nullable = False)
     rating = db.Column(db.Float)
 
     def __repr__(self):
@@ -29,10 +30,21 @@ class Review(db.Model):
     game_id = db.Column(db.Integer, db.ForeignKey('game.game_id'), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable =  False) 
     review_content = db.Column(db.Text, nullable = False)
-    hours_played = db.Column(db.Float, nullable = False) 
+    hours_played = db.Column(db.Integer, nullable = False) 
     completed = db.Column(db.Boolean, nullable = True) 
     rating = db.Column(db.Integer, nullable = False) #must be between 0 - 100 inclusive
-    review_created_at = db.Column(db.DateTime, nullable = False) 
+    review_created_at = db.Column(db.DateTime, nullable = False, default = datetime.now(timezone.utc)) 
     
     game = db.relationship('Game', backref=db.backref('reviews', lazy=True))
     user = db.relationship('User', backref=db.backref('reviews', lazy=True))
+
+class UserGame(db.Model):
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), primary_key=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('game.game_id'), primary_key=True)
+    added_at = db.Column(db.DateTime, nullable=False, default = datetime.now(timezone.utc))
+     
+    game = db.relationship('Game', backref=db.backref('user_games', lazy=True))
+    user = db.relationship('User', backref=db.backref('user_games', lazy=True))
+
+    def __repr__(self):
+        return f'<UserGame {self.user_id}:{self.game_id}>'
