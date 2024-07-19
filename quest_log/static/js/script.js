260e58e08715e16c775685eccbc5459fcf5ad607
Paul Tapping <zombieplayground@gmail.com>
2024-07-19 21:06:49 +0100

    BUILD: game details page

--- quest_log/static/js/script.js
+++ quest_log/static/js/script.js
@@ -12,3 +12,8 @@
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems);
+  });
